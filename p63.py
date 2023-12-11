cc44d807efe5c5d40b9fca41a67f41261a9f4c9d
tcl326 <lintingche2012@gmail.com>
2023-12-11 16:34:27 +0800

    p63

---
+++ p63.py
@@ -0,0 +1,13 @@
+
+def main():
+    res = 0
+    for i in range(1, 100 + 1):
+        for j in range(1, 100 + 1):
+            if len(str(i ** j)) == j:
+                print(i ** j)
+                res += 1
+    return res
+
+
+if __name__ == "__main__":
+    print(main())
