79f3aab0dfd96e1aab5f1fa66c5455d6619b2078
tcl326 <lintingche2012@gmail.com>
2023-12-13 15:05:29 +0800

    continued fractions convergent

--- lib/continued_fractions.py
+++ lib/continued_fractions.py
@@ -41,5 +41,42 @@ def sqrt(s: int) -> Tuple[List[int], List[int]]:
     return a_list
 
 
+def fraction(a: int, b: int) -> Tuple[List[int], List[int]]:
+    a_list = []
+    if a < b:
+        a, b = b, a
+        a_list.append(0)
+    r = a % b
+    while r != 0:
+        a_list.append(a // b)
+        a = b
+        b = r
+        r = a % b
+    a_list.append(a // b)
+    return a_list, []
+
+
+def convergent(a: List[int]) -> Tuple[int, int]:
+    """
+    https://pi.math.cornell.edu/~gautam/ContinuedFractions.pdf
+
+    based on theorem Theorem 2.4.
+    """
+    assert a, 'a cannot be empty'
+    p_0 = a[0]
+    q_0 = 1
+    if len(a) == 1:
+        return p_0, q_0
+    p_1 = a[1] * a[0] + 1
+    q_1 = a[1]
+    for i in range(2, len(a)):
+        p_n = a[i] * p_1 + p_0
+        q_n = a[i] * q_1 + q_0
+        p_0, q_0, p_1, q_1 = p_1, q_1, p_n, q_n
+    return p_1, q_1
+
+
 if __name__ == '__main__':
     print(sqrt(13))
+    print(fraction(43, 19))
+    print(convergent([2, 3, 1, 4]))
